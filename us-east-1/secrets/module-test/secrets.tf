4c5376ba9070054c105b45a9f6241e5764554687
ss027503 <srs6c7@gmail.com>
2022-04-27 23:03:33 -0500

    source path syntax

--- us-east-1/secrets/module-test/secrets.tf
+++ us-east-1/secrets/module-test/secrets.tf
@@ -4,12 +4,9 @@ locals {
 }
 
 module gen_secret {
-    source = "git::https://github.com/ss027503/terraform-modules.git//aws/gen-secret"
+    source = "git@ss027503.github.com:ss027503/terraform-modules.git//aws/gen-secret"
 
     secret_name = "some-test-secret"
     length = 18
     special = false
 }
-
-
-  
